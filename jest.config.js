24426ed08552a1bf3a445cf25b15a33572275685
evgenid <evgenid@jfrog.com>
2023-04-19 11:07:52 +0300

    JPAAS-000 - Jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: [
+    "**/src/**/*.(spec|test|e2e).ts",
+    "**/test/**/*.(spec|test|e2e).ts",
+  ]
 };
--- test/single-layer.e2e.ts
+++ test/single-layer.e2e.ts
@@ -1,5 +1,7 @@
 import Cache from '../src/Cache';
 
 describe('Single level', function () {
-
+    it('should ', function () {
+        expect(true).toBe(true);
+    });
 });
